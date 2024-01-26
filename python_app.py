cda7a9b405270bd3bc2d8d1feeb31cc666c375b3
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-01-25 21:46:14 -0500

    Add multiply function

--- python_app.py
+++ python_app.py
@@ -5,3 +5,7 @@ def add(a, b):
 def subtract(a, b):
 	answer = a - b
 	print(str(a) + '-' + str(b) + ' = ' + str(answer))
+ 
+def multiply(a, b):
+	answer = a * b
+	print(str(a) + '*' + str(b) + ' = ' + str(answer))
