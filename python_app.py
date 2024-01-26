31cfdd0f7c4db0bd6a92b89bac49c539f9d774f8
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-01-25 21:57:08 -0500

    Finish project by creating all necessary conditions

--- python_app.py
+++ python_app.py
@@ -9,3 +9,43 @@ def subtract(a, b):
 def multiply(a, b):
 	answer = a * b
 	print(str(a) + '*' + str(b) + ' = ' + str(answer))
+ 
+def divide(a, b):
+	answer = a / b
+	print(str(a) + '/' + str(b) + ' = ' + str(answer))
+ 
+print('A. Addition')
+print('B. Subtraction')
+print('C. Multiplication')
+print('D. Division')
+print('E. Exit')
+
+choice = input('Input your choice: ')
+
+if choice == 'a' or choice == 'A':
+        print('Addition')
+        a = int(input('Input first number: '))
+        b = int(input('Input second number: '))
+        add(a, b)
+
+elif choice == 'b' or choice == 'B':
+        print('Subtraction')
+        a = int(input('Input first number: '))
+        b = int(input('Input fisecondrst number: '))
+        subtract(a, b)
+
+elif choice == 'c' or choice == 'C':
+        print('Multiplication')
+        a = int(input('Input first number: '))
+        b = int(input('Input fisecondrst number: '))
+        multiply(a, b)
+elif choice == 'd' or choice == 'D':
+        print('Division')
+        a = int(input('Input first number: '))
+        b = int(input('Input fisecondrst number: '))
+        divide(a, b)
+elif choice == 'e' or choice == 'E':
+        print('Program ended')
+        quit()
+
+
